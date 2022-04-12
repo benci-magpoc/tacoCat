e21bfd4d2e699bd04d544f68ad3c0ee9e54521ac
Benci Magpoc <BENCIIAN@GMAIL.COM>
2022-04-12 15:49:05 -0400

    finished algorithm

--- js/site.js
+++ js/site.js
@@ -1,29 +1,62 @@
-function getMessage() {
-    let message = document.getElementById("txtMessage").value;
-    displayMessage(message);
+function getString() {
+    let message = document.getElementById("stringInput").value;
+
+    //checks if input is null
+    if (message != "") {
+        //convert to lower case and take out spac
+        message = message.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+
+        //assigning reversedString result to reversedString to pass onto view
+        let reversedString = reverseStringB(message);
+
+        //calling view and passing the reversed input
+        displayString(reversedString, checkPalindrome(message, reversedString));
+
+    } else {
+        alert("Please input a valid string!");
+    }
 }
 
+//View model
+function displayString(message, isPalindrome) {
 
-function displayMessage(message) {
-
-    // let item = `<li class="list-group-item">${message}</li>`;
-
-    // getting ol element from the page
     element = document.getElementById("results");
+    let item = document.createElement("div");
 
-    // create a new li element
-    let item = document.createElement("li");
-    //add class to the element
-    item.classList.add("list-group-item");
-    item.classList.add("messageClass");
-
-    //set the message for the li element
-    item.innerHTML = message;
-    // add new item to list
+    //change innerHTML to the original input and whethere it is palindrome or not
+    item.innerHTML = `${message} ${isPalindrome}`;
     element.appendChild(item);
+}
 
+//function to reverse string
+function reverseString(message) {
+    let reversedString = "";
 
-    // element.insertAdjacentHTML("beforeend", item);
-    let clearForm = document.getElementById("txtMessage");
-    clearForm.innerHTML = "";
+    for (i = message.length - 1; i >= 0; i--) {
+        let char = message[i];
+        //concatenante reversedString with the character from the index of message
+        reversedString += char;
+        //convert to lower caps
+    }
+    console.log(reversedString);
+    console.log(message);
+    return reversedString;
+}
+
+//algorithm using built in functions
+function reverseStringB(message) {
+    let reversedString = "";
+
+    reversedStringArray = message.split("");
+    reversedString = reversedStringArray.reverse().join("");
+    return reversedString;
+}
+
+//algorithm to check if string is palindrome
+function checkPalindrome(message, reversedString) {
+    if (message == reversedString) {
+        return "is a palindrome";
+    } else {
+        return "is not a palindrome";
+    }
 }
